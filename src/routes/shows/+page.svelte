c2357dcc860d94152e9ad8ee325edc08722ea53f
Pranav Tripathi <pranav02.tripathi@gmail.com>
2023-10-24 10:14:03 +0530

    minor changes

--- src/routes/shows/+page.svelte
+++ src/routes/shows/+page.svelte
@@ -1,1 +1,15 @@
-<div>shows</div>
+<script>
+  // const timer = 0;
+  // console.log(timer);
+</script>
+
+<div>
+  <h1>Shows</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur neque dicta
+    possimus dolor id ab! Rerum excepturi asperiores voluptate recusandae.
+  </p>
+</div>
+
+<style>
+</style>
--- src/routes/watchlist/+page.svelte
+++ src/routes/watchlist/+page.svelte
@@ -1,1 +1,15 @@
-<div>watchlist</div>
+<script>
+  // const timer = 0;
+  // console.log(timer);
+</script>
+
+<div>
+  <h1>Watchlist</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur neque dicta
+    possimus dolor id ab! Rerum excepturi asperiores voluptate recusandae.
+  </p>
+</div>
+
+<style>
+</style>
