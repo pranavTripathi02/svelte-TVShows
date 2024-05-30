c82fee64add6c8f95c39f76713a5314fa25093bd
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-31 05:29:35 +0530

    Change Header component
    redesign
    update styles for tailwindcss

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,88 +1,39 @@
-<script>
-</script>
-
-<header>
-  <nav>
-    <a class="nav-item" href="/">
+<header class="bg-background-nav flex w-full justify-center gap-8 py-4">
+  <nav class="flex justify-center">
+    <a class="px-3 py-2" href="/">
       <i class="show-logo fa-solid fa-film" />
       Home
     </a>
+    <a class="px-3 py-2" href="/">
+      <i class="show-logo fa-solid fa-film" />
+      Movies
+    </a>
+    <a class="px-3 py-2" href="/">
+      <i class="show-logo fa-solid fa-film" />
+      TV Shows
+    </a>
+    <a class="px-3 py-2" href="/">
+      <i class="show-logo fa-solid fa-film" />
+      Watch
+    </a>
   </nav>
-  <div class="search-bar">
-    <input type="text" placeholder="Search for TV Shows" />
+  <div
+    class="bg-background-secondary outline-accent-2 group flex items-center rounded-md px-4 outline-1 focus-within:outline"
+  >
+    <input
+      type="text"
+      placeholder="Search CeeTV"
+      class="w-full bg-transparent outline-none"
+    />
+    <!-- search icon -->
   </div>
-  <nav>
-    <a class="nav-item" href="/shows">Shows</a>
-    <a class="nav-item" href="/watchlist">Watchlist</a>
+  <nav class="flex items-center">
+    <a class="nav-item" href="/watchlist">
+      <div class="flex gap-2">
+        <!-- bookmark icon -->
+        <span>Watchlist</span>
+      </div>
+    </a>
+    <!-- theme menu -->
   </nav>
 </header>
-
-<style>
-  header {
-    display: flex;
-    width: 100%;
-    justify-content: space-between;
-    align-items: center;
-    /* padding: 0 50px; */
-    margin: 0 0;
-    /* border: 1px solid red; */
-    box-sizing: border-box;
-    height: 3rem;
-    border-bottom: 1px solid rgba(var(--rgbprimary), 0.5);
-  }
-  header > * {
-    margin: 0 0.5rem;
-  }
-  nav {
-    display: flex;
-    /* width: 75%; */
-    justify-content: space-between;
-  }
-  .search-bar {
-    width: 30%;
-  }
-  input {
-    width: 100%;
-    padding: 5px;
-    border-radius: 5px;
-    outline: none;
-    border: none;
-  }
-  input:focus {
-    outline: 1px solid var(--accent);
-  }
-  a {
-    color: var(--text);
-    text-decoration: none;
-  }
-  a:hover {
-    color: var(--accent);
-  }
-  .nav-item {
-    padding: 0.8rem;
-    margin: 0 5px;
-    position: relative;
-  }
-  .nav-item:hover {
-    cursor: pointer;
-    border-radius: 5px;
-    background-color: rgba(var(--rgbsecondary), 0.8);
-    transition: ease-in-out;
-    transition-duration: 150ms;
-  }
-  .show-logo {
-    display: none;
-  }
-
-  @media screen and (min-width: 1024px) {
-    header > * {
-      margin: 0 2rem;
-    }
-    .search-bar {
-      width: 50%;
-    }
-    .show-logo {
-      display: inline-block;
-    }
-  }
-</style>
