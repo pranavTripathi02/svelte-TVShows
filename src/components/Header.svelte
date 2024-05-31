80ad3b5380b2ee92195b9b8f6b5fa20692788fe7
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-31 06:47:07 +0530

    Add more changes to Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,39 +1,50 @@
-<header class="bg-background-nav flex w-full justify-center gap-8 py-4">
-  <nav class="flex justify-center">
-    <a class="px-3 py-2" href="/">
-      <i class="show-logo fa-solid fa-film" />
-      Home
-    </a>
-    <a class="px-3 py-2" href="/">
-      <i class="show-logo fa-solid fa-film" />
-      Movies
-    </a>
-    <a class="px-3 py-2" href="/">
-      <i class="show-logo fa-solid fa-film" />
-      TV Shows
-    </a>
-    <a class="px-3 py-2" href="/">
-      <i class="show-logo fa-solid fa-film" />
-      Watch
-    </a>
-  </nav>
-  <div
-    class="bg-background-secondary outline-accent-2 group flex items-center rounded-md px-4 outline-1 focus-within:outline"
-  >
-    <input
-      type="text"
-      placeholder="Search CeeTV"
-      class="w-full bg-transparent outline-none"
-    />
-    <!-- search icon -->
-  </div>
-  <nav class="flex items-center">
-    <a class="nav-item" href="/watchlist">
-      <div class="flex gap-2">
-        <!-- bookmark icon -->
-        <span>Watchlist</span>
+<header class="bg-background-navw-full py-2">
+  <div class="container mx-auto flex justify-center gap-12">
+    <nav class="flex justify-center">
+      <a class="decoration-accent px-3 py-2 hover:underline" href="/"> Home </a>
+      <a class="decoration-accent px-3 py-2 hover:underline" href="/">
+        Movies
+      </a>
+      <a class="decoration-accent px-3 py-2 hover:underline" href="/">
+        TV Shows
+      </a>
+      <a class="decoration-accent px-3 py-2 hover:underline" href="/">
+        Watch
+      </a>
+    </nav>
+    <div
+      class="bg-background-secondary outline-accent group flex items-center rounded-md px-4 outline-1 focus-within:outline"
+    >
+      <input
+        type="text"
+        placeholder="Search CeeTV"
+        class="w-full bg-transparent py-0 outline-none"
+      />
+      <!-- search icon -->
+      <button
+        on:click={(e) => e.preventDefault()}
+        class="h-full w-8"
+        title="Search"
+      >
+        <i
+          class="fa-solid fa-search text-text-secondary group-focus-within:text-text-primary py-0"
+        /></button
+      >
+    </div>
+    <nav class="flex items-center gap-8">
+      <a class="nav-item" href="/watchlist">
+        <div class="group flex gap-2">
+          <!-- bookmark icon -->
+          <i
+            class="fa-solid fa-bookmark group-hover:text-accent fa-lg m-auto"
+          />
+          <span class="decoration-accent group-hover:underline">Watchlist</span>
+        </div>
+      </a>
+      <div>
+        <i class="fa-solid fa-sun group-hover:text-accent fa-lg m-auto" />
       </div>
-    </a>
-    <!-- theme menu -->
-  </nav>
+      <!-- theme menu -->
+    </nav>
+  </div>
 </header>
