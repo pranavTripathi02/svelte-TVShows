c98a21f8240217ec1eb0839b2f5258641c242d40
Pranav Tripathi <pranav02.tripathi@gmail.com>
2024-05-31 06:47:48 +0530

    add show type

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export type TShow = {
+  id: number;
+  url: string;
+  title: string;
+  type: string;
+  language: string;
+  genres: string[];
+  status: string; // "Ended"|
+  runtime: number;
+  averageRuntime: number;
+  premiered: string;
+  ended: string;
+  officialSite: string;
+  schedule?: { time: string; days: string[] };
+  rating: { average: number };
+  weight: number;
+  network: {
+    id: number;
+    name: string;
+    country: { name: string; code: string; timezone: string };
+    officialSite: string;
+  };
+  webChannel?: string;
+  dvdCountry?: string;
+  externals?: { tvrage?: number; thetvdb?: number; imdb?: number };
+  image: { medium?: string; original?: string };
+  summary: string;
+  updated: number;
+  _links: {
+    self: { href: string };
+    previousepisode: { href: string; name: string };
+  };
+};
