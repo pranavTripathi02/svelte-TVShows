b09f34dc890074e0b9d7b6ef3b5ec351ad9aabf0
Pranav Tripathi <pranav02.tripathi@gmail.com>
2025-05-18 00:02:52 +0530

    fix: show details home

--- src/routes/shows/[showId]/+page.svelte
+++ src/routes/shows/[showId]/+page.svelte
@@ -1,90 +1,107 @@
 <script lang="ts">
-  import Loading from "../../../components/Loading.svelte";
-  import Details from "./Details.svelte";
-  import SuggestionsSidebar from "../../../components/SuggestionsSidebar.svelte";
-  import TopInfo from "./TopInfo.svelte";
-  import Template from "../Template.svelte";
+  import type { PageProps } from "./$types";
+  import EpisodesSlot from "./episodesSlot.svelte";
+  import ImageSlot from "./imageSlot.svelte";
+  import CastSlot from "./castSlot.svelte";
+  import LoadingIcon from "../../../assets/icons/loading_icon.svelte";
 
-  let { data } = $props();
-  let {
+  let { data }: PageProps = $props();
+
+  let showSummary = $state(false);
+</script>
+
+{#if data.name}
+  {@const {
     name,
-    averageRuntime,
     genres,
     image,
     network,
     premiered,
     status,
     summary,
-    rating,
-  } = $derived(data?.showInfo);
-  let { showImages, showCast, showSeasons } = $derived(data);
-  let backImages = $derived(showImages.filter((item) => item.type === "background"));
-</script>
+    _embedded,
+    ended,
+  } = data}
+  <div class="top">
+    <h1 class="text-4xl">{name}</h1>
+    <span class="text-text-200/80"
+      >{premiered}<span class="text-text-200/50">—</span
+      >{status.toLowerCase() == "ended" ? ended : "Ongoing"}</span
+    >
+    <div class="genre-tabs mt-2 flex flex-wrap gap-2">
+      {#each genres as genre}
+        <a
+          href={`shows/categories/${genre}`}
+          class="border-text-500 bg-background-300/50 hover:bg-background-200 hover:border-accent-300/50 my-0 flex min-h-[40px] min-w-[40px] items-center overflow-clip rounded-full border px-4 py-2 leading-4 text-nowrap capitalize duration-200 lg:min-h-max"
+          >{genre}</a
+        >
+      {/each}
+    </div>
+    <div class="main-images my-12">
+      {#if _embedded?.images.length!!}
+        {@const backImages = _embedded?.images.filter(
+          (item) => item.type === "background",
+        )}
 
-<!-- {#if data.loading} -->
-<!--   <Loading /> -->
-<!-- {:else} -->
-<!--   <div class="grid-container"> -->
-<!--     <div class="top"> -->
-<!--       <TopInfo data={{ name, image, backImages, genres }} /> -->
-<!--     </div> -->
-<!--     <div class="col1"> -->
-<!--       <Details data={{ summary, network, showSeasons, showImages, showCast }} /> -->
-<!--     </div> -->
-<!--     <div class="col2"> -->
-<!--       <SuggestionsSidebar /> -->
-<!--     </div> -->
-<!--   </div> -->
-<!--   <!-- <RelatedSlot related={shows}/> -->
-<!-- {/if} -->
-
-<Template>
-  {#snippet top()}
-    <TopInfo  data={{ name, image, backImages, genres }} />
-  {/snippet}
-  {#snippet content()}
-    <Details
-      
-      data={{ summary, network, showSeasons, showImages, showCast }}
-    />
-  {/snippet}
-  {#snippet sidebar()}
-    <SuggestionsSidebar  />
-  {/snippet}
-</Template>
-
-<style>
-  /* .grid-container { */
-  /*   display: grid; */
-  /*   grid-template-areas: */
-  /*     "top top" */
-  /*     "col1 col2"; */
-  /*   grid-template-columns: 1.5fr 1fr; */
-  /*   grid-gap: 1rem; */
-  /* } */
-  /* .grid-container > * { */
-  /*   overflow: hidden; */
-  /* } */
-  /* .top { */
-  /*   grid-area: top; */
-  /* } */
-  /* .col1 { */
-  /*   grid-area: col1; */
-  /*   padding: 0.5rem; */
-  /* } */
-  /* .col2 { */
-  /*   grid-area: col2; */
-  /*   padding: 1rem; */
-  /* } */
-  /* @media screen and (max-width: 1024px) { */
-  /*   .grid-container { */
-  /*     grid-template-areas: */
-  /*       "top" */
-  /*       "col1"; */
-  /*     grid-template-columns: auto; */
-  /*   } */
-  /*   .col2 { */
-  /*     display: none; */
-  /*   } */
-  /* } */
-</style>
+        {#if image}
+          <img src={image?.medium} alt="main-poster" />
+        {/if}
+        {#each backImages as showImg}
+          <img
+            src={showImg.resolutions?.original?.url ||
+              showImg.resolutions.medium.url}
+            alt="show posters"
+          />
+        {/each}
+        <!-- <img src={backImages[0]} alt="" /> -->
+      {/if}
+    </div>
+    <div class="my-10">
+      {#if network?.name}
+        <a class="show-actions-action available text-lg" href="/"
+          >Available on: <span class=" text-accent-300 p-2"
+            >{network?.name}</span
+          ></a
+        >
+      {/if}
+      <span class="hidden">Add to watchlist</span>
+    </div>
+  </div>
+  <div class="flex-container">
+    <!-- Summary -->
+    {#if summary!!}
+      <div class="bg-background-300/50 mt-6 h-full rounded-lg px-4 py-2">
+        <span class="block text-lg font-medium opacity-60">Summary</span>
+        <button
+          class="group text-left"
+          onclick={() => {
+            showSummary = !showSummary;
+          }}
+        >
+          <p class:line-clamp-3={!showSummary}>
+            {@html summary}
+          </p>
+          <span
+            class="text-text-100/50 mt-3.5 text-left font-medium group-hover:underline"
+          >
+            See {showSummary ? "less" : "more"}
+          </span>
+        </button>
+      </div>
+    {/if}
+    <EpisodesSlot seasonList={_embedded?.seasons} />
+    <ImageSlot imageList={_embedded?.images} />
+    <CastSlot castList={_embedded?.cast} />
+  </div>
+  <!-- <div class="flex-container"> -->
+  <!--   <h3>More</h3> -->
+  <!--   <h5>Explore by genre</h5> -->
+  <!--   <h5>Explore by people</h5> -->
+  <!--   <h5>Explore by networks</h5> -->
+  <!--   <h5>Explore your watchlist</h5> -->
+  <!-- </div> -->
+{:else}
+  <div class="h-full w-full text-center">
+    <LoadingIcon class="m-auto animate-spin" width="48" height="48" />
+  </div>
+{/if}
--- src/routes/shows/[showId]/+page.ts
+++ src/routes/shows/[showId]/+page.ts
@@ -1,77 +1,16 @@
-export async function load({ fetch, params }) {
-  const showId = params.showId;
-  let data = { loading: true };
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({ fetch, params }) => {
   const fetchShowInfo = async () => {
-    const showInfo = await fetch(`https://api.tvmaze.com/shows/${showId}`).then(
-      (res) => res.json(),
-    );
-    data.loading = false;
-    return showInfo;
+    return await fetch(
+      `https://api.tvmaze.com/shows/${params.showId}?embed[]=cast&embed[]=images&embed[]=seasons`,
+    )
+      .then(async (res) => {
+        return await res.json();
+      })
+      .catch((err) =>
+        console.error("Something went wrong. Could not fetch show info", err),
+      );
   };
-  const fetchShowImages = async () => {
-    const showImages = await fetch(
-      `https://api.tvmaze.com/shows/${showId}/images`,
-    ).then((res) => res.json());
-    data.loading = false;
-    return showImages;
-  };
-  const fetchShowCast = async () => {
-    const showCast = await fetch(
-      `https://api.tvmaze.com/shows/${showId}/cast`,
-    ).then((res) => res.json());
-    data.loading = false;
-    return showCast;
-  };
-  const fetchShowSeasons = async () => {
-    const showSeasons = await fetch(
-      `https://api.tvmaze.com/shows/${showId}/seasons`,
-    ).then((res) => res.json());
-    data.loading = false;
-    return showSeasons;
-  };
-  return {
-    showInfo: fetchShowInfo(),
-    showImages: fetchShowImages(),
-    showCast: fetchShowCast(),
-    showSeasons: fetchShowSeasons(),
-  };
-}
-// export const fetchShow = async () => {
-//     const pageNum = Math.ceil(Math.random() * 249);
-//     const data = {
-//         shows: [],
-//         topTen: [],
-//         categories: [],
-//         featuredShows: [],
-//         popularTen: [],
-//     };
-//     try {
-//         const res = await fetch(`https://api.tvmaze.com/shows?page=${pageNum}`);
-//         await res
-//             .json()
-//             .then((raw) => {
-//                 data.shows = raw.sort((a, b) => b?.weight - a?.weight);
-//
-//                 data.featuredShows = data.shows?.slice(0, 10);
-//
-//                 data.popularTen = data.shows?.slice(0, 10);
-//
-//                 data.topTen = data.shows
-//                     .sort((a, b) => b?.rating?.average - a?.rating?.average)
-//                     .slice(0, 10);
-//
-//                 for (let i = 0; i < data.shows.length; i++) {
-//                     data.shows[i].genres?.filter((item) => {
-//                         const j = data.categories.findIndex((x) => x == item);
-//                         if (j <= -1) data.categories.push(item);
-//                     });
-//                 }
-//             })
-//             .catch((err) => console.error(err));
-//         showStore.set({ ...data, loading: false });
-//     } catch (err) {
-//         console.error("error loading data", err);
-//     }
-// };
-// fetchShows();
-// // await fetchShows();
+  return await fetchShowInfo();
+};
