a593fead1cc653df49fe2c43bc026eeb8abe35aa
Pranav Tripathi <pranav02.tripathi@gmail.com>
2025-05-18 00:28:23 +0530

    fix: show details cast section

--- src/routes/shows/[showId]/castSlot.svelte
+++ src/routes/shows/[showId]/castSlot.svelte
@@ -1,76 +1,37 @@
 <script lang="ts">
+  import SectionHeader from "../../../components/SectionHeader.svelte";
+
   let { castList } = $props();
 </script>
 
-<h2 class="title"><a class="title-link" href="/">Top Cast</a></h2>
-<div class="cast-list">
-  {#each castList as cast}
-    <div class="cast-info">
-      <a href="/"> <span class="link"></span></a>
-      <img src={cast.person?.image?.medium} alt="" class="cast-info-photo" />
-      <div class="cast-info-name">{cast.person.name}</div>
-      <div class="cast-info-character">
-        <span style="opacity: .8;">as</span>
-        {cast.character.name}
-      </div>
-    </div>
-  {/each}
-</div>
-
-<style>
-  .title-link {
-    color: var(--text);
-    text-decoration: none;
-  }
-  .title-link:hover {
-    color: var(--accent);
-    text-decoration: underline;
-  }
-  .cast-list {
-    display: grid;
-    grid-template-columns: repeat(4, minMax(10rem, 12rem));
-    /* grid-gap: auto; */
-    align-items: center;
-    overflow: hidden;
-  }
-  .cast-info {
-    display: flex;
-    flex-direction: column;
-    margin: 1rem;
-    align-items: center;
-    text-align: center;
-    position: relative;
-  }
-  .cast-info:hover {
-    color: var(--accent);
-  }
-  .cast-info-character {
-    opacity: 0.8;
-    font-size: 0.9rem;
-  }
-  .cast-info-photo {
-    margin: 1rem;
-    border-radius: 50%;
-    height: 8rem;
-    width: 8rem;
-    object-fit: cover;
-    object-position: top;
-  }
-  .link {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    z-index: 1;
-  }
-  @media screen and (max-width: 1024px) {
-    .cast-list {
-      display: flex;
-      margin: 0 5rem 0 0;
-      width: 100%;
-      /* overflow: hidden; */
-      /* border: 1px solid red; */
-    }
-  }
-</style>
+<section class="title mt-12 text-2xl">
+  <SectionHeader title="Top Cast" />
+  <div
+    class="mx-auto my-8 grid w-full grid-cols-2 place-content-center justify-items-center gap-8 overflow-hidden sm:grid-cols-3 xl:grid-cols-4"
+  >
+    {#each castList as cast}
+      <a
+        href="#"
+        aria-label={`Open page for ${cast.person.name}`}
+        class="group flex flex-col items-center"
+      >
+        <div>
+          <img
+            src={cast.person?.image?.medium}
+            alt=""
+            class="aspect-square size-full h-24 w-24 rounded-full object-cover object-center duration-300 group-hover:opacity-80 lg:h-32 lg:w-32"
+          />
+        </div>
+        <div class="mt-2 text-center text-lg group-hover:underline">
+          {cast.person.name}
+        </div>
+        <div class="text-base">
+          <span class="opacity-70">as</span>
+          <span class="text-accent-400 group-hover:text-accent-300"
+            >{cast.character.name}</span
+          >
+        </div>
+      </a>
+    {/each}
+  </div>
+</section>
