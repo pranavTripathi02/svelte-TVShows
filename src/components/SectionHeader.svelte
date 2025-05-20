d414c92bcada5886e159453a57ca8bc5c7aec9bf
Pranav Tripathi <pranav02.tripathi@gmail.com>
2025-05-20 23:17:30 +0530

    fix: update section header

--- src/components/SectionHeader.svelte
+++ src/components/SectionHeader.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-  const { title }: { title: string } = $props();
+  import type { HTMLAttributes } from "svelte/elements";
+  const {
+    title,
+    ...rest
+  }: { title: string } & HTMLAttributes<HTMLHeadingElement> = $props();
 </script>
 
-<h2 class="relative text-2xl font-medium">
+<h2 class="relative text-2xl font-medium" {...rest}>
   <span
     class="before:bg-accent-300 before:absolute before:inset-0 before:-left-2 before:my-auto before:h-3/5 before:w-1"
   ></span>
